40d6d4d35494bbf49da76db7e9e02a5db9361b63
Alaina-gl <108637123+Alaina-gl@users.noreply.github.com>
2025-10-21 10:11:37 -0700

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Alaina Ge on 2025-10-21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
